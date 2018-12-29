3f56ff85dfdf16c6083cb0cb55e095c9323f5aae
Nguyen Quang An <ngquangan@gmail.com>
2018-12-29 21:20:50 +0700

    Resolved reverse string

---
+++ src/reverse-string.js
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/reverse-string
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseString = function(s) {
+    return s.split("").reverse().join("");  
+};
