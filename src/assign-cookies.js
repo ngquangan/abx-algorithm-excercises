6c45628b7b627921707c6e6ff7a79616cbb2c98e
Nguyen Quang An <ngquangan@gmail.com>
2019-01-05 19:41:14 +0700

    Resolved assign cookies

---
+++ src/assign-cookies.js
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/assign-cookies
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    g.sort((a,b) => a - b);
+    s.sort((a,b) => a - b);
+    let i = 0, j = 0, count = 0;
+    while( i < g.length && j < s.length) {
+        if(s[j] >= g[i]) {
+            count++;
+            i++;
+            j++;
+        } else {
+            j++;
+        }
+    }
+    return count;
+};
