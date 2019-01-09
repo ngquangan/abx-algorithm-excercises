f5f098e335c2605105e596e62f4141529144bae4
Nguyen Quang An <ngquangan@gmail.com>
2019-01-09 23:36:56 +0700

    Resolved 132 Pattern

---
+++ src/132-pattern.js
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/132-pattern
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var find132pattern = function(nums) {
+    let min = Number.MAX_VALUE;
+    for(let j = 0 ; j < nums.length - 1; j++) {
+        min = Math.min(min, nums[j]);
+        for(let k = j + 1; k < nums.length; k++) {
+            if(nums[j] > nums[k] && min < nums[k]) return true;
+        }
+    }
+    return false;
+};
