6fa4ea6592b19bafb2230bec3aa1f1826ff40fb0
ngquangan <ngquangan@gmail.com>
2019-01-12 12:20:32 +0700

    updated base 7

--- src/base-7.js
+++ src/base-7.js
@@ -4,5 +4,14 @@
  * @return {string}
  */
 var convertToBase7 = function(num) {
-    return num.toString(7);
+    if(num === 0) return "0";
+    let s = "";
+    let temp = num;
+    num = num < 0 ? -num : num;
+    while(num) {
+        s = (num%7) + s;
+        num = parseInt(num/7);
+    }
+   if(temp < 0) return "-"+ s;
+    return s;
 };
